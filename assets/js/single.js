540d671cc1e82ab43994521d6b1b78de8544ed07
Steven Cruz <cruz801@hotmail.com>
2021-12-01 00:42:46 -0700

    redirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,12 +1,29 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search
+    var repoName = queryString.split("=")[1];   
 
+    if(repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+    
+}
 
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    //var repoName = queryString.split("=")[1];
     fetch(apiUrl).then(function(response) {
         //request was successful
         if (response.ok) {
@@ -21,13 +38,14 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");;
         }
     })
-    console.log(repo);
+    
   };
   
-  getRepoIssues("facebook/react");
+  
 
   var displayIssues = function(issues) {
       if (issues.length === 0) {
@@ -72,3 +90,5 @@ var getRepoIssues = function(repo) {
       limitWarningEl.appendChild(linkEl);
   
   };
+   getRepoName()
+   //getRepoIssues("facebook/react");
